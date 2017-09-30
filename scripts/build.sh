66eb9a19c7d2c2fef9ba7cbde9c8d172416ea7c6
Leon Sodhi <leon@papertrailapp.com>
2017-09-30 03:32:36 +0100

    Packaging

--- scripts/build.sh
+++ scripts/build.sh
@@ -30,6 +30,7 @@ __proj_dir="$(dirname "$__dir")"
 
 plugin_name=${__proj_dir##*/}
 build_dir="${__proj_dir}/build"
+pkg_dir="${__proj_dir}/pkg"
 go_build=(go build -ldflags "-w")
 
 _info "project path: ${__proj_dir}"
@@ -41,8 +42,25 @@ export CGO_ENABLED=0
 _debug "removing: ${build_dir:?}/*"
 rm -rf "${build_dir:?}/"*
 
+_debug "removing: ${pkg_dir:?}/*"
+rm -rf "${pkg_dir:?}/"*
+
 _info "building plugin: ${plugin_name}"
 export GOOS=linux
 export GOARCH=amd64
 mkdir -p "${build_dir}/${GOOS}/x86_64"
 "${go_build[@]}" -o "${build_dir}/${GOOS}/x86_64/${plugin_name}" . || exit 1
+
+# build package for the Intel agent
+mkdir -p pkg/tmp/opt/snap/plugins
+cp -f "${build_dir}/${GOOS}/x86_64/${plugin_name}" pkg/tmp/opt/snap/plugins
+(cd ${pkg_dir} && \
+fpm -s dir -C tmp -t deb \
+  -n ${plugin_name} \
+  -m "Papertrails Ops <ops@papertrail.com>" \
+  -v 5 \
+  -d snap-telemetry \
+  --url "https://www.papertrail.com" \
+  --description "DBI plugin for the Intel snap agent" \
+  --vendor "Papertrail" \
+  opt/snap/plugins/snap-plugin-collector-dbi)
